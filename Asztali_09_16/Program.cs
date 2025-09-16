e7f72cb73174634bab86d159e3393164f9618314
Rendszergazda <rendszergazda@blathy.info>
2025-09-16 14:01:12 +0200

    commit

--- Asztali_09_16/Program.cs
+++ Asztali_09_16/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            /*
             // 1.feladta
             Console.WriteLine("Hello, World!");
            
@@ -83,23 +84,106 @@
                 Console.WriteLine($"A legkisebb szám: {szam7}");
             }
             Console.WriteLine("------------");
+            /*
+            
+
+
             // 7. feladat
 
             // 8. feladat
 
+            
+
+
+            /*
             // 9. feladat
+            Console.WriteLine("Adj meg egy számot");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adj meg egy számot");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adj meg egy számot");
+            int c = int.Parse(Console.ReadLine());
+            int D = b * b - 4 * a * c;
+            if (D >= 0) 
+            {
+                Console.WriteLine("Van megoldása");
+            }
+            else if (D < 0) 
+            {
+                Console.WriteLine("Nincs megoldása");
+            }
+            
 
             // 10. feladat
-
+            Console.WriteLine("Adj meg egy számot");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adj meg egy számot");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adj meg egy számot");
+            int c = int.Parse(Console.ReadLine());
+            int D = b * b - 4 * a * c;
+            if (D >= 0)
+            {
+                Console.WriteLine($"Az egynelet megoldása: {Math.Sqrt(D)}");
+            }
+            else if (D < 0)
+            {
+                Console.WriteLine("Nincs megoldása");
+            }
+           
             // 11. feladat
+            Console.WriteLine("Adj meg a háromszög befogóját");
+            double a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adj meg a háromszög másik befogóját");
+            double b = int.Parse(Console.ReadLine());              
+            double atfogo = Math.Sqrt(a * a + b * b);
+
+            Console.WriteLine($"Az átfogó: {atfogo:F2}");
+
+            
 
             // 12. feladat
+            Console.WriteLine("Téglatest egyik éle:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Téglatest másik éle:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Téglatest harmadik éle:");
+            int c = int.Parse(Console.ReadLine());
 
-            // 13. feladat
+            int terfogat = a * b * c;
+            int felszin = 2 * (a * b + a * c + b * c);
+
+            Console.WriteLine($"A télatest térfogata: {terfogat:F2}");
+            Console.WriteLine($"A télatest felszíne: {felszin:F2}");
+            
+          // 13. feladat
+
+          Console.WriteLine("A kör átmérője:");
+            int atmero = int.Parse(Console.ReadLine());
+            int sugar = atmero / 2;
+
+            int kerulet = atmero * (int)Math.PI;
+            int terulet = sugar * sugar * (int)Math.PI;
+
+            Console.WriteLine($"A kör kerülete: {kerulet:F2}");
+            Console.WriteLine($"A télatest területe: {terulet:F2}");
+
+              
 
             // 14. feladat
-            Console.WriteLine();
 
+            Console.WriteLine("Körív sugara:");
+            int sugara = int.Parse(Console.ReadLine());
+            Console.WriteLine("Középponti szöge:");
+            int szoge = int.Parse(Console.ReadLine());
+
+            int korivTerulet = szoge / 360  * (sugara * 2) * (int)Math.PI;
+            int hataroloIv = szoge / 360 * (sugara * sugara) * (int)Math.PI;
+
+            Console.WriteLine($"A körív területe: {korivTerulet:F2}");
+            Console.WriteLine($"A határoló ív hossza: {hataroloIv:F2}");
+
+            */
         }
     }
 }

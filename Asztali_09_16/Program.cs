8974d0a75e299fc4579fff761a3d81a25ec6f69c
Rendszergazda <rendszergazda@blathy.info>
2025-09-16 12:35:00 +0200

    commit

--- Asztali_09_16/Program.cs
+++ Asztali_09_16/Program.cs
@@ -41,6 +41,50 @@
             {
                 Console.WriteLine(szam2 - szam1);
             }
+            Console.WriteLine("------------");
+
+            // 5.feladat
+
+            Console.WriteLine("Adj meg egy számot");
+            int szam3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adj meg egy számot");
+            int szam4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("------------");
+
+            if (szam3 > szam4) {
+                Console.WriteLine(szam3);
+            }
+            else
+            {
+                Console.WriteLine(szam4);
+            }
+            Console.WriteLine("------------");
+
+            // 6.feladat
+
+            Console.WriteLine("Adj meg egy számot");
+            int szam5 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adj meg egy számot");
+            int szam6 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adj meg egy számot");
+            int szam7 = int.Parse(Console.ReadLine());
+            Console.WriteLine("------------");
+
+            if (szam5 < szam6 && szam5 < szam7)
+            {
+                Console.WriteLine(szam5);
+            }
+            else if (szam6 < szam5 && szam6 < szam7) {
+                
+                Console.WriteLine(szam6);
+            }
+            else
+            {
+                Console.WriteLine(szam7);
+            }
+            Console.WriteLine("------------");
+            // 7. feladat
+
 
         }
     }

af14dc8abb81859b1f343df2798965ea93a8ff60
Rendszergazda <rendszergazda@blathy.info>
2025-09-16 12:10:21 +0200

    Add project files.

---
+++ Asztali_09_16/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Asztali_09_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
